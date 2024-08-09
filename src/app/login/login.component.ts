97de6894c0adac821863793d8a64b416da75e2ae
Karla Anchondo <karla.anchondo@gmail.com>
2024-08-09 13:02:56 -0600

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+   /* NgModule, */ 
+    CommonModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [AuthService],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login({ email: this.email, password: this.password }).subscribe(response => {
+      if (response.success) {
+        // Redirige al usuario a la página principal o muestra un mensaje de éxito
+      } else {
+        // Muestra un mensaje de error
+      }
+    });
+  }
+}
